807b839bc0246c529dcef208c892f86614a81693
SimonSuslich <simonniklas.suslich@elev.ga.ntig.se>
2024-11-11 14:46:53 +0100

    Push

--- app.rb
+++ app.rb
@@ -1,7 +1,91 @@
 class App < Sinatra::Base
 
+    # Funktion som returnerar en databaskoppling
+    # Exempel på användning: db.execute('SELECT * FROM fruits')
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/todos.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
+
+    
+    # Routen gör en redirect till '/fruits'
     get '/' do
+        redirect("/todos")
+    end
+
+
+    #Routen hämtar alla frukter i databasen
+    get '/todos' do
+        @not_completed_todos = db.execute('SELECT * FROM todos WHERE status_complete = false ORDER BY id')
+        @completed_todos = db.execute('SELECT * FROM todos WHERE status_complete = true ORDER BY id')
         erb(:"index")
     end
+
+    post '/todos' do 
+        title = params['title']
+        description = params['description']
+        category = params['category']
+        due_date = params['due_date']
+        
+        db.execute("INSERT INTO todos (title, description, category, due_date, post_date) VALUES(?,?,?,?,?)", [title, description, category, due_date, "CURRENT_TIMESTAMP"])
+        redirect("/")
+
+    end
+
+    # # Övning no. 2.1
+    # # Routen visar ett formulär för att spara en ny frukt till databasen.
+    # get '/fruits/new' do 
+    #     erb(:"fruits/new")
+    # end
+
+    # # Övning no. 2.2
+    # # Routen sparar en frukt till databasen och gör en redirect till '/fruits'.
+    # post '/fruits' do 
+    #     p params
+    #     #todo
+    # end
+
+    # # Övning no. 1
+    # # Routen visar en frukt.
+    # get '/fruits/:id_num' do | id_num |
+      
+    #     @fruit = db.execute('SELECT * FROM fruits WHERE id=?', id_num).first
+   
+    #     erb(:"fruits/show")
+    # end
+
+    # post '/fruits/:id/delete' do | id |
+    #     #todo: ta bort frukten med idt
+    #     db.execute('DELETE FROM fruits WHERE id = ?', id)
+    #     redirect("/fruits")        
+    # end
+
+
+
+
+    # get '/fruits/:id/edit' do |id|
+    #     @fruit = db.execute('SELECT * FROM fruits WHERE id=?', id).first
+    #     erb(:"fruits/edit")
+    # end
+        
+
+    # post '/fruits/:id/update' do |id|
+
+
+    #     name = params['name']
+    #     tastiness = params['tastiness']
+    #     description = params['description']
+        
+    #     db.execute("UPDATE fruits SET name=?, description=?, tastiness=? WHERE id=?", [[name, tastiness, description], id])
+    #     redirect("/fruits")
+
+    # end
+
+
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,11 +1,55 @@
+# require 'sqlite3'
+
+# class Seeder
+
+#   def self.seed!
+#     p "doit"
+#   end
+
+# end
+
+# Seeder.seed!
+
 require 'sqlite3'
 
 class Seeder
-
+  
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todos')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                description TEXT,
+                category TEXT,
+                due_date DATETIME,
+                post_date DATETIME,
+                status_complete BOOL)')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO todos (title, description, category, due_date, post_date, status_complete) VALUES ("Thing 1", "Lala lblab bla", "Kategory", "2024-02-22", "2024-02-22", false)')
+    db.execute('INSERT INTO todos (title, description, category, due_date, post_date, status_complete) VALUES ("Thing 2", "To do bla", "Kategory", "2024-02-22", "2024-02-22", false)')
+    db.execute('INSERT INTO todos (title, description, category, due_date, post_date, status_complete) VALUES ("Thing 3", "CHmoki chmoki mi was ochen lyubin da tak sho vi nas uzhe zadolbali", "Kategory", "2024-02-22", "2024-02-22", true)')
+  end
+
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todos.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
 
 Seeder.seed!
